90dd29e8d107ef36bce9270c64284fbad603b118
Joao Pedro <joaopedrowatson@hormail.com>
2019-12-19 17:05:07 -0300

    Reoorganizando

--- config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-define("DB_SERVER",     "localhost:3306");
-define("DB_USER",       "root");
-define("DB_PASS",       "killmeplzA1#@");
-define("DB_NAME",       "study");
-define("GMAIL_USER",    "watsonpythonguy@gmail.com");
-define("GMAIL_PASS",    "thanks4dat");
-define("GMAIL_NAME",    "Watson's Productions") 
-?>
